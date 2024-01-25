657f42ba4fbd64758b79e1a1cea4a444143aa510
DeltaProjects <programmingwithfarhan@gmail.com>
2024-01-25 17:05:50 +0530

    Fixing errors

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
  const Login = () => {
 
